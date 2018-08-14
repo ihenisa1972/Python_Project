cb124c78c8023f99f1c18c884ef4a30a60921241
Ian Henisa <ihenisa@salesforce.com>
2018-08-13 20:56:35 -0400

    added math stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,3 +5,17 @@ print ("hello world")
 These are comments also
 '''
 
+#Print Let's Do Math
+
+variableX = 0
+variableY = 0
+
+variableX: int = 5 * 5
+variableY: int = 2 + 2
+summation: int = variableX + variableY
+
+print (variableX)
+print (variableY)
+#Print ("The value of variableX and variableY added together is: " summation)
+
+
