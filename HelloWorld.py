5be7b73c23ff62b1ea3653c338780c90bd90ebd6
Ian Henisa <ihenisa@salesforce.com>
2018-08-20 21:31:27 -0400

    More Stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,21 +1,49 @@
-#Print Hello World
-print ("hello world")
 
-'''
-These are comments also
-'''
-
-#Print Let's Do Math
-
-variableX = 0
-variableY = 0
-
-variableX: int = 5 * 5
+variableX: int = 5
 variableY: int = 2 + 2
-summation: int = variableX + variableY
 
-print (variableX)
-print (variableY)
-#Print ("The value of variableX and variableY added together is: " summation)
+#Print For loop
+
+'''for i in range(5):
+    variableX = variableX * 5
+    print(variableX)
+ 
+
+if variableY > 4:
+    print("Greater than 4")
+else:
+    print("Not greater than 4")
+
+
+
+while variableX != 0:
+    print(variableX)
+    variableX -= 1
+    
+    
+brand = 'Apple'
+exchangeRate = 1.235235245
+
+message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR' % (brand, 1299, exchangeRate)
+
+print (message)
+
+
+
+test = 'Variable1 is %d, variable2 is %d' % (5, 10)
+
+print (test)
+
+'''
+x = 0
+
+while x<5:
+    print('Hello')
+    x = x+1
+
+
+
+
+
 
 
