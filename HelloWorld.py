e84af9c1b8bcbd2c09853a7e85eee9426b6739b4
Ian Henisa <ihenisa@salesforce.com>
2018-09-10 18:12:23 -0400

    Updates

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,49 +1,15 @@
-
-variableX: int = 5
-variableY: int = 2 + 2
-
-#Print For loop
-
-'''for i in range(5):
-    variableX = variableX * 5
-    print(variableX)
- 
-
-if variableY > 4:
-    print("Greater than 4")
-else:
-    print("Not greater than 4")
+import math
 
 
-
-while variableX != 0:
-    print(variableX)
-    variableX -= 1
-    
-    
-brand = 'Apple'
-exchangeRate = 1.235235245
-
-message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR' % (brand, 1299, exchangeRate)
-
-print (message)
+def square(x):
+    return x * x
 
 
-
-test = 'Variable1 is %d, variable2 is %d' % (5, 10)
-
-print (test)
-
-'''
-x = 0
-
-while x<5:
-    print('Hello')
-    x = x+1
+print (math.sqrt(25))
+print (square(5))
 
 
-
-
-
-
+def main():
+    print('hello')
+    return
 
