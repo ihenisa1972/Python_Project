e959352757cb79655791e6dc8f41f5ff82388bea
ihenisa <ihenisa1972@gmail.com>
2019-01-04 20:51:25 -0500

    plain text email update code

--- SendingEmails/EmailFile1.py
+++ SendingEmails/EmailFile1.py
@@ -1,11 +1,25 @@
 import smtplib, ssl
+from email.mime.txt import MIMEText
+from eail.mime.multipart import MIMEMultipart
 
-port = 465 # For SSL
+# Setting up secured connection
+
+sender_email = "ihenisa1972@gmail.com"
+receiver_email = "ihenisapython@gmail.com"
 password = input("Type your password and press enter: ")
+
+
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+port = 465  # For SSL
+
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("my@gmail.com",password)
-    # TODO: Send email here
+    server.login("ihenisapython@gmail.com",password)
+    server.sendmail(sender_email, receiver_email, message)
