dacbca8bd34d39891c49080ff98dbfb55e51e586
ihenisa <ihenisa1972@gmail.com>
2019-01-12 19:44:31 -0500

    Class Implementation Updates

--- SendingEmails/EmailFile1.py
+++ SendingEmails/EmailFile1.py
@@ -6,11 +6,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # Create Secure Connection Class
-class SecureConnection:
+class Email:
 
     # Initializer
-    def __init__(self):
-        pass
+    def __init__(self, password):
+        sender_email = "ihenisa1972@gmail.com"
+        receiver_email = "ihenisapython@gmail.com"
 
     # Secure Connection
     def CreateConnection(self, sender_email, receiver_email, message):
@@ -19,128 +20,82 @@ class SecureConnection:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
 
-# Simple Email
-class SimpleEmail:
-
-    # Initializer
-    def __init__(self):
-        pass
-
-    # Email text
+    # Simple Email
     def CreateEmailText(self):
         message = """\
         Subject: Hi there
 
         This message is sent from Python."""
 
-# MIME Email Class
-class MimeEmail:
+    # MIME Email
+    def MimeEmail(self):
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "multipart test"
+        message["From"] = sender_email
+        message["To"] = receiver_email
 
-    # Initializer
-    def __init__(self):
-        pass
+    # Plain-text and HTML version
+    def PlainTextAndHTMLVersion(self):
+        text = """\
+        Hi,
+        How are you?
+        Real Python has many great tutorials:
+        www.realpython.com"""
+        html = """\
+        <html>
+          <body>
+            <p>Hi,<br>
+               How are you?<br>
+               <a href="http://www.realpython.com">Real Python</a> 
+               has many great tutorials.
+            </p>
+          </body>
+        </html>
+        """
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "multipart test"
-    message["From"] = sender_email
-    message["To"] = receiver_email
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
 
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = """\
-    <html>
-      <body>
-        <p>Hi,<br>
-           How are you?<br>
-           <a href="http://www.realpython.com">Real Python</a> 
-           has many great tutorials.
-        </p>
-      </body>
-    </html>
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
 
-# Email Attachement Class
-class EmailAttachment:
+    # Email with Attachment
+    def EmailAttachment(self):
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email  # Recommended for mass emails
 
-    # Initializer
-    def __init__(self):
-        pass
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        filename = "document.txt"  # In same directory as script
+
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
 
 
 if __name__ == "__main__":
-    pass
-# Setting up secured connection
-'''
-sender_email = "ihenisa1972@gmail.com"
-receiver_email = "ihenisapython@gmail.com"
-password = input("Type your password and press enter: ")
-'''
-
-subject = "An email with attachment from Python"
-body = "This is an email with attachment sent from Python"
-sender_email = "ihenisa1972@gmail.com"
-receiver_email = "ihenisapython@gmail.com"
-password = input("Type your password and press enter:")
-
-# Simple Text
-
-
-# MIME email format.  HTML Content
-'''
-"""
-
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
-
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
-
-port = 465  # For SSL
-
-# Create secure connection with server and send email
-
-'''
-
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-message["Bcc"] = receiver_email  # Recommended for mass emails
-
-# Add body to email
-message.attach(MIMEText(body, "plain"))
-
-filename = "document.txt"  # In same directory as script
-
-# Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
-
-# Encode file in ASCII characters to send by email
-encoders.encode_base64(part)
-
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
-
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
-
-# Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+    password = input("Type your password and press enter: ")
