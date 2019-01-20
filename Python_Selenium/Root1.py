7b022d6212977af1195caf1ad0e0bd88b61c94ae
ihenisa <ihenisa1972@gmail.com>
2019-01-19 23:11:59 -0500

    boom

--- Python_Selenium/Root1.py
+++ Python_Selenium/Root1.py
@@ -1,6 +1,5 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-opts = Options()
 
 
 opts = Options()
@@ -11,10 +10,7 @@ search_form.click()
 search_form = browser.find_element_by_link_text('Standings')
 search_form.click()
 
-'''search_form.send_keys('ihenisa')
-search_form = browser.find_element_by_id('password-field')
-search_form.send_keys('Jehovah1914')
-search_form.submit()'''
+
 
 
 
